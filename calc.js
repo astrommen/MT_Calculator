dc6275ab5f79f96c2acbcbf8674f2b129b4c01fe
Aja Strommen <a.g.mechanix@gmail.com>
2021-03-02 23:58:17 -0500

    grabbing input values

---
+++ calc.js
@@ -0,0 +1,13 @@
+// Todo
+// grab all number values from text input
+// grab value of operator value from radio btns
+// 
+var getValue = ()=>{
+
+  var first_number = document.getElementById('first-number').value;
+  var second_number = document.getElementById('second-number').value;
+  var operator = document.querySelector('input[name="operator"]:checked').value;
+  console.log(first_number, second_number, operator);
+
+}
+
