01b1d726ea61f8eaf28fad58a7f3b6e8379d7ad2
Aja Strommen <a.g.mechanix@gmail.com>
2021-03-03 22:17:22 -0500

    calculator works

--- calc.js
+++ calc.js
@@ -2,15 +2,34 @@
 // grab all number values from text input
 // grab value of operator value from radio btns
 // 
-var getValue = ()=>{
-
-  var first_number = document.getElementById('first-number').value;
-  var second_number = document.getElementById('second-number').value;
-  var operator = document.querySelector('input[name="operator"]:checked').value;
-  console.log(first_number, second_number, operator);
-
+var resetCalc = () => {
+  document.getElementById('content').reset();
 }
 
-var resetCalc = ()=> {
-  document.getElementById('content').reset();
+let calc = () => {
+
+  var num1 = parseInt(document.getElementById('first-number').value);
+  var num2 = parseInt(document.getElementById('second-number').value);
+  var operator = document.querySelector('input[name="operator"]:checked').value;
+  let result = 0;
+
+  if (operator === "add") {
+    result = num1 + num2;
+  }
+
+  else if (operator === "subtract") {
+    result = num1 - num2;
+  }
+
+  else if (operator === "multiply") {
+    result = num1 * num2;
+  }
+
+  else if (operator === "divide") {
+    result = num1 / num2;
+  }
+
+  console.log(result);
+  // let resultTest = document.getElementByID('result-text')
+  // resultTest.innerhtml = "The result of " + num1 + "and " + num2 + "= " + result;
 }
